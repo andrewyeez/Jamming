3769de1374ad82d2b64c2a03327aa4777f590860
Andrew Yee <andrewyee18331@sbcglobal.net>
2018-03-20 21:25:46 -0700

    create and implement SearchResults

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        {/**<!-- Add a TrackList component -->*/}
+      </div>
+    );
+  }
+}
+
+export default SearchResults;
